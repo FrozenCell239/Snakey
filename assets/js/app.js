3408c7d28e3421c86fd05637be014b08859fbb22
FrozenCell239 <val8ioks@gmail.com>
2024-09-01 21:02:36 +0200

    Front-end improvements - Removed the user cursor on the snake grid. - Replaced the JS' alert and prompt with a dialog/modal. - Added some comments. - Minor changes and fixes.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,16 +7,20 @@ const apple = new Fruit();
 const score_zone = document.getElementById('score');
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const keyset_select = document.getElementById('keyset_select');
+const keyset_select = document.getElementById('keyset-select');
+const gameover_dialog = document.getElementById('gameover');
+const gameover_message = document.getElementById('gameover-message');
+const gameover_button = document.getElementById('gameover-button');
 let keyset = ['Z', 'Q', 'S', 'D'];
 let score = 0;
 let count = 0;
 let players;
 let least_best_score;
 
-async function getFakePlayers(min_score = 2, max_score = 6){
+// Getting fake players and their scores for tests
+async function getFakePlayers(min_score = 6, max_score = 18){
     try{
-        const response = await fetch('https://randomuser.me/api/?results=30');
+        const response = await fetch('https://randomuser.me/api/?results=20');
         const data = await response.json();
         let new_players = [];
 
@@ -33,6 +37,7 @@ async function getFakePlayers(min_score = 2, max_score = 6){
     };
 };
 
+// Updating displayed scoreboard
 function updateScoreboard(){
     const scoreboard = document.getElementById('scoreboard');
     const number_of_players = 20;
@@ -69,6 +74,24 @@ keyset_select.addEventListener('change', function(event){
     };
     document.activeElement.blur(); // Removes focus from the keyset selector in order to prevent a bug.
 });
+
+// Pushing new highscore to the scoreboard
+window.newHighscore = function (){
+    const username = document.getElementById('username').value;
+
+    if(username === ""){
+        alert("You must enter a valid username to save your score and continue.");
+        return;
+    };
+    players.push({
+        username: username,
+        score: score
+    });
+    players.sort((a, b) => b.score - a.score);
+    localStorage.setItem('snake_players', JSON.stringify(players));
+    updateScoreboard();
+    return location.reload();
+};
 
 // Game loop (not starting it yet)
 function loop(){
@@ -126,18 +149,17 @@ function loop(){
             // Snake eats itself, so it dies
             if(cell.x === snake.cells[i].x && cell.y === snake.cells[i].y){
                 if(score > least_best_score){
-                    players.push({
-                        username: prompt("Enter your username : "),
-                        score: score
-                    });
-                    players.sort((a, b) => b.score - a.score);
-                    localStorage.setItem('snake_players', JSON.stringify(players));
-                    updateScoreboard();
+                    gameover_message.innerText = `You scored ${score} points. Enter your username to save your score and continue.`;
+                    gameover_button.innerText = "Save and try again !";
+                    gameover_button.getAttributeNode('onclick').value = `newHighscore(${score})`;
                 }
                 else{
-                    alert("Game over !");
+                    document.getElementById('username').remove();
+                    gameover_message.innerText = `You scored ${score} points. Another chance ?`;
+                    gameover_button.innerText = "Try again !";
+                    gameover_button.getAttributeNode('onclick').value = 'location.reload()';
                 };
-                location.reload();
+                gameover_dialog.showModal();
             };
         };
     });
